fc37e9bb3b59044ea51b2e9fc0d34ca1cb3363a8
Valeria Saponaro <v.saponaro97@gmail.com>
2023-09-19 09:36:59 +0200

    Solo Esercizio1

---
+++ main.js
@@ -0,0 +1,42 @@
+console.dir(document);
+// Esercizio primo 
+
+// let comando1 = document.querySelector(`.bottone1`);
+// comando1.style.backgroundColor= "blue"
+
+// let paragrafhs = document.querySelectorAll("p");
+
+// comando1.addEventListener("click", () => {
+//     paragrafhs.forEach ((el) => {
+//         el.classList.toggle("d-none");
+//     })
+// });
+
+// let comando2 = document.querySelector(`.bottone2`);
+// comando2.style.backgroundColor= "red"
+// let check= false;
+// comando2.addEventListener("click", () => {
+//     if(check == false){
+//    paragrafhs.forEach ((paragrafo) => {
+//     paragrafo.style.color= `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)} )`;
+//     })
+//     check = true;
+//     } else {
+//         paragrafhs.forEach ((paragrafo) => {
+//             paragrafo.style.color= `black`;
+//             })
+//         check = false;
+//     }
+// });
+
+
+// let comando3 = document.querySelector(`.bottone3`);
+// comando3.style.backgroundColor= "green"
+// comando3.addEventListener("click", () => {
+//     paragrafhs.forEach ((el) => {
+//         el.classList.toggle("fw-bold");
+//     })
+// });
+
+// Secondo esercizio 
+
